5b4c0584cbf9dbfbb80990cd4bfc8f18a5fd6cb9
Бугаев Роман <roman15082002@mail.ru>
2025-10-28 16:22:25 +0300

    feat: добавляет ip ядра  для блоков памяти fpga

---
+++ ip_cores/ram1p1rwbe/ram1p1rwbe_inst.v
@@ -0,0 +1,8 @@
+ram1p1rwbe	ram1p1rwbe_inst (
+	.address ( address_sig ),
+	.byteena ( byteena_sig ),
+	.clock ( clock_sig ),
+	.data ( data_sig ),
+	.wren ( wren_sig ),
+	.q ( q_sig )
+	);
---
+++ ip_cores/rom1p1r_/rom1p1r__inst.v
@@ -0,0 +1,6 @@
+rom1p1r_	rom1p1r__inst (
+	.address ( address_sig ),
+	.clken ( clken_sig ),
+	.clock ( clock_sig ),
+	.q ( q_sig )
+	);
