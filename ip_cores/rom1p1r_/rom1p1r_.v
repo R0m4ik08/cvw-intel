95972cd997bbdcd22e1ae3c99982edfc9437d6db
Бугаев Роман <roman15082002@mail.ru>
2025-11-12 14:46:58 +0300

    fix(qsim): исправляет инициализацию данных rom модуля для симуляции

--- ip_cores/rom1p1r_/rom1p1r_.v
+++ ip_cores/rom1p1r_/rom1p1r_.v
@@ -91,7 +91,13 @@ module rom1p1r_
 		altsyncram_component.address_aclr_a = "NONE",
 		altsyncram_component.clock_enable_input_a = "NORMAL",
 		altsyncram_component.clock_enable_output_a = "NORMAL",
+`ifdef QARTUS_PRJ
 		altsyncram_component.init_file = "../zsbl/bin/boot.mif",
+`endif 
+`ifndef QARTUS_PRJ
+		altsyncram_component.init_file = "./boot.mif",
+`endif 
+
 		altsyncram_component.intended_device_family = "Cyclone IV E",
 		altsyncram_component.lpm_hint = "ENABLE_RUNTIME_MOD=NO",
 		altsyncram_component.lpm_type = "altsyncram",
